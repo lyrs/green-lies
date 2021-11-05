c79d6c47860d8fe363a9a5c4ddbf63623ffe7b5d
Paul-Alexandre Fourrière <pa.fourriere@gmail.com>
2021-11-05 16:17:52 +0100

    Create file to get hrefs and scrape the data to a database

---
+++ hrefsToCorpus.py
@@ -0,0 +1,38 @@
+import urllib.request
+from bs4 import BeautifulSoup
+from tinydb import TinyDB
+import csv
+
+def item(url):
+    with urllib.request.urlopen(url) as page_source:
+        page = page_source.read().decode('utf-8')
+        soup = BeautifulSoup(page, features="html.parser").find('section', id='productPage')
+
+    # scraping
+    entry = {}
+
+    title = soup.find(class_='prdct__designation')
+    entry['brand'] = title.find(class_='prdct__name').text
+    entry['name'] = title.find(attrs={'itemprop': 'name'}).text
+
+    entry['labels'] = [label.text.strip() for label in soup.find(class_="prdct__labels").find_all('li')]
+
+    details = soup.find(class_="prdct__details-wrap")
+    entry['description'] = details.find(id='description').text
+    entry['ingredients'] = details.find(id='ingredients').text
+
+    return entry
+
+
+db = TinyDB('db.json')
+
+with open('nocibe-minced-meat/hrefs-nocibe.csv', newline='') as csvfile:
+    nocibe_csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    nocibe_csv_reader.__next__()
+    for row in nocibe_csv_reader:
+        print(row[0])
+        db.insert(item(row[0]))
+        print(nocibe_csv_reader.line_num)
+
+
+
