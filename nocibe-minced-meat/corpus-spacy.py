b9cb0a17759d88e5bb636610dcac54dd4a71a175
thebugcreator <duyngovan1404@gmail.com>
2021-11-29 16:05:06 +0100

    Script for creating a wordcloud

--- nocibe-minced-meat/corpus-spacy.py
+++ nocibe-minced-meat/corpus-spacy.py
@@ -2,6 +2,10 @@ import spacy
 from tinydb import TinyDB
 from spacy.tokens import DocBin
 # from spacy.tokens import Doc
+from spacy.tokenizer import Tokenizer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 
 # --------------------------------------------------
 
@@ -41,3 +45,59 @@ def export_spacy_corpus():
     doc_bin.to_disk("data/corpus.spacy")
 
 
+# def remove_stop_words(dir):
+#     with open(dir, encoding="utf-8") as sentencestxt:
+#         sentences = sentencestxt.readlines()
+#     pipeline = spacy.load("fr_core_news_sm")
+#     stop_words = pipeline.Defaults.stop_words
+#     purged_sentences = []
+#     for sentence in sentences:
+#         text_tokens = sentence.split(" ")
+#         purged_sentence = [token.strip() for token in text_tokens if token not in stop_words]
+#         purged_sentences.append(purged_sentence)
+#     return purged_sentences
+#
+#
+# def purify_and_merge_sentences(dir):
+#     purged_sentences = remove_stop_words(dir)
+#     words = []
+#     for sentence in purged_sentences:
+#         [words.append(word) for word in sentence]
+#     return words
+#
+#
+# def create_word_cloud():
+#     words = purify_and_merge_sentences("data/sentences_for_tagging.txt")
+#     wc = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(" ".join(words))
+#     plt.figure()
+#     plt.imshow(wc, interpolation="bilinear")
+#     plt.axis("off")
+#     plt.show()
+
+def load_from_txt(dir):
+    sentences = []
+    results = []
+    with open(dir, encoding="utf-8") as file:
+        sentences = file.readlines()
+    pipeline = spacy.load("fr_core_news_sm")
+    docs = [pipeline(sentence) for sentence in sentences]
+    stop_words = pipeline.Defaults.stop_words
+    for doc in docs:
+        [results.append(token) for token in doc if token.text.lower() not in stop_words]
+    return results
+
+
+# results = load_from_txt("data/sentences_for_tagging.txt")
+
+def create_word_cloud():
+    tokens = load_from_txt("data/sentences_for_tagging.txt")
+    words = []
+    for token in tokens:
+        words.append(token.text)
+    wc = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(" ".join(words))
+    plt.figure()
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+create_word_cloud()
